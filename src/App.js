eab32df37300702de1fda828966498e8b0cf425a
paulrcook <paul@lougeek.com>
2017-07-13 16:20:02 -0400

    Added a few components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RecipeList from './components/RecipeList.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>hey</h1>
+        <RecipeList/>
+        <button onClick={this.handleClick}>
+          Add Recipe
+        </button>
       </div>
     );
   }
---
+++ src/components/RecipeList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class RecipeList extends React.Component {
+    render() {
+        return <div>RecipeList</div>
+    }
+}
+
+export default RecipeList;
